507d7566d99b631278422430164d034a8c2d81ea
yujin-zero <poly30@naver.com>
2024-07-10 16:59:59 +0900

    [ADD] BidController 에 get 기능 추가

--- src/main/java/com/example/usedauction/service/BidService.java
+++ src/main/java/com/example/usedauction/service/BidService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class BidService {
@@ -27,4 +28,7 @@ public class BidService {
     }
 
 
+    public List<Bid> getAllBids() {
+        return bidRepository.findAll();
+    }
 }
