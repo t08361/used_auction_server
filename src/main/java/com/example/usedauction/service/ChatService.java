785bd1580799bd7c0fcc4f7f15b2cb81cd90f056
yujin-zero <poly30@naver.com>
2024-07-24 14:48:14 +0900

    [feat] 채팅방의 마지막 메시지와 시간을 업데이트하는 엔드포인트 추가
    - ChatController에 /updateLastMessage 엔드포인트 추가
    - ChatService에 updateLastMessage 메서드 구현
    - 메시지 저장 후 updateLastMessage 호출하도록 sendMessage 메서드 수정

--- src/main/java/com/example/usedauction/service/ChatService.java
+++ src/main/java/com/example/usedauction/service/ChatService.java
@@ -7,7 +7,10 @@ import com.example.usedauction.repository.ChatRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ChatService {
@@ -18,7 +21,10 @@ public class ChatService {
     private ChatRoomRepository chatRoomRepository;
 
     public ChatMessage sendMessage(ChatMessage chatMessage) {
-        return chatMessageRepository.save(chatMessage);
+        ChatMessage savedMessage = chatMessageRepository.save(chatMessage);
+        Date lastMessageTime = Date.from(chatMessage.getTimestamp().atZone(ZoneId.systemDefault()).toInstant());
+        updateLastMessage(chatMessage.getChatRoomId(), chatMessage.getContent(), lastMessageTime);
+        return savedMessage;
     }
 
     public ChatRoom createChatRoom(ChatRoom chatRoom) {
@@ -30,5 +36,25 @@ public class ChatService {
     }
     public List<ChatRoom> getChatRooms() {
         return chatRoomRepository.findAll();
+    }
+
+    public void updateLastMessage(String chatRoomId, String lastMessage, Date lastMessageTime) {
+        // chatRoomId에 해당하는 ChatRoom을 데이터베이스에서 찾음
+        Optional<ChatRoom> optionalChatRoom = chatRoomRepository.findById(chatRoomId);
+
+        // 해당 ChatRoom이 존재할 경우
+        if (optionalChatRoom.isPresent()) {
+            // ChatRoom 객체를 가져옴
+            ChatRoom chatRoom = optionalChatRoom.get();
+
+            // ChatRoom의 마지막 메시지와 마지막 메시지 시간을 업데이트
+            chatRoom.setLastMessage(lastMessage);
+            chatRoom.setLastMessageTime(lastMessageTime);
+
+            // 업데이트된 ChatRoom 객체를 데이터베이스에 저장
+            chatRoomRepository.save(chatRoom);
+        } else {
+            throw new RuntimeException("ChatRoom not found");
+        }
     }
 }
